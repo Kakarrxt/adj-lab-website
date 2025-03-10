e770d55d7ba6b998e30e66623a9213abb60bca8b
Kakarrxt <kakarrot.goku2003@gmail.com>
2025-03-11 03:40:52 +0530

    no need for query

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,13 +1,11 @@
 'use client';
 import React from 'react';
 import AppContextProvider from './AppContextProvider';
-import ReactQueryClientProvider from './ReactQueryClientProvider';
 import SessionProvider from './SessionProvider';
 import SuspenseProvider from './SuspenseProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        <ReactQueryClientProvider>
             <SuspenseProvider>
                 <AppContextProvider>
                     <SessionProvider>
@@ -15,7 +13,6 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
                     </SessionProvider>
                 </AppContextProvider>
             </SuspenseProvider>
-        </ReactQueryClientProvider>
     )
 }
 
