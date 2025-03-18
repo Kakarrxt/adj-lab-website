cab82b7e7b5ff41a05597c3c5979efb9689a99ea
Kakarrxt <kakarrot.goku2003@gmail.com>
2025-03-18 16:53:13 +0530

    fix css import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,9 @@ import "./members/members.module.css";
 import "./publications/Publications.module.css";
 import "./research/Research.module.css";
 import "./page.module.css";
+import "../components/Masonry/Masonry.css";
+import "../components/AnimatedTitle/AnimatedTitle.module.css";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from "next/head";
